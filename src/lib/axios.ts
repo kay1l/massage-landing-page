c343f2c5140f6df5acaff64775578e4babbf1d7c
Kay1l <142229866+kay1l@users.noreply.github.com>
2025-08-14 02:47:29 +0800

    new update in the axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "/api/proxy",
+  baseURL: "/api",
   timeout: 10000,
   headers: {
     // "Content-Type": "application/json",
