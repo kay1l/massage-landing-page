ebb0eb1c368334e698b350c5a87584a0be7e6138
Kay1l <142229866+kay1l@users.noreply.github.com>
2025-08-15 15:44:37 +0800

    fix

--- src/types/booking.d.ts
+++ src/types/booking.d.ts
@@ -9,16 +9,24 @@ export interface BookingPayload {
   }
   
   export interface BookingResponse {
-  booking_date_time: string;
-  full_name: string;
-  contact_number: string;
-  address: string;
-  landmark?: string;
-  notes_or_preferences?: string;
-  service_id: number;
-  updated_at: string;
-  created_at: string;
-  id: number;
+    booking_date: string;
+    booking_time: string,
+    full_name: string;
+    contact_number: string;
+    address: string;
+    landmark?: string;
+    notes_or_preferences?: string;
+    updated_at: string;
+    created_at: string;
+    id: number;
+    user_id: number | NULL;
+    service: Service;
+    status: string;
+  }
+
+  export interface Service {
+    id: number,
+    name: string
   }
 
   export interface BookingApiResponse {
